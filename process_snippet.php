1e86f39bfebe95182cc648a5db7a01eff82e7d10
aftamat4ik <aftamat4ik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-09 22:12:58 +0000

    get code added
    git-svn-id: https://plugins.svn.wordpress.org/code-snippets-extended/trunk@1085894 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- process_snippet.php
+++ process_snippet.php
@@ -31,21 +31,19 @@ function proc_shortcode($data){
 	preg_match_all('#<\?php([\s\S]+?)\?>#im',$code,$e_php);
 	if($e_php){
 		foreach($e_php[1] as $key=>$tmp){
-			foreach($e_php[1] as $key=>$tmp){
-				$res = "";
-				ob_start(); // Ловим вывод eval'а в основной буфер вывода
-				/**
-				 * 
-				 * Если вдруг вас интересует - как работают вложенные друг в друга ob_start, то вот тема на стековерфлов - http://stackoverflow.com/questions/10441410/what-happened-when-i-use-multi-ob-start-without-ob-end-clean-or-ob-end-flush
-				 * 
-				 */
+			$res = "";
+			ob_start(); // Ловим вывод eval'а в основной буфер вывода
+			/**
+			 * 
+			 * Если вдруг вас интересует - как работают вложенные друг в друга ob_start, то вот тема на стековерфлов - http://stackoverflow.com/questions/10441410/what-happened-when-i-use-multi-ob-start-without-ob-end-clean-or-ob-end-flush
+			 * 
+			 */
 
-				eval($tmp);
-				$res = ob_get_contents();
-				ob_clean();
-				ob_end_flush();
-				$code = str_replace ( "<?php".$tmp."?>" , $res , $code);
-			}
+			eval($tmp);
+			$res = ob_get_contents();
+			ob_clean();
+			ob_end_flush();
+			$code = str_replace ( "<?php".$tmp."?>" , $res , $code);
 		}
 	}
 
